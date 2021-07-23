cdd1784a52c5101a8236b4ccb384a83ca1f14aba
Stefan Katic <katicstefan.coder@gmail.com>
2021-07-23 10:45:58 +0200

    first commit

---
+++ src/main/java/com/apurs/microservices/placesservice/PlacesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.placesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlacesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlacesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apurs/microservices/placesservice/PlacesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apurs.microservices.placesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlacesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
